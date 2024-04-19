94659f7732d3c1a53cba1cef7b07a740eeede711
Rahul Kalantre <rahulkalantre97@gmail.com>
2024-04-19 13:52:26 +0530

    [ + [ Added Footer ] ]
    - Added Footer to the landing pages.
    - And completed the Responsive Landing Page Project with proper responsiveness.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import FeatureSection from "./components/FeatureSection";
 import WorkFlow from "./components/WorkFlow";
 import Pricing from "./components/Pricing";
 import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -16,9 +17,10 @@ const App = () => {
         <WorkFlow />
         <Pricing />
         <Testimonials />
+        <Footer />
       </div>
     </>
   );
-};
+}; 
 
 export default App;
